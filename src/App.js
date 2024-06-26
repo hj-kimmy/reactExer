c304d7686dbc363d9fc0c5b116929aa87e825fd0
hj-kimmy <hjsmile0406@gmail.com>
2024-06-26 21:25:35 +0900

    helloreact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MyComponent from './MyComponents';
+import Counter from './Counter';
+import EventPractice from './EventPractice';
+import EventPractice2 from './EventPractice2';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+import Say from './Say';
+import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <MyComponent name="뤼액트" favoriteNumber={1}>
+          리액트
+        </MyComponent>
+        <Counter />
+        <Say></Say>
+        <EventPractice></EventPractice>
+        <EventPractice2></EventPractice2>
+        <ValidationSample></ValidationSample>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)}></ScrollBox>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+        <br />
+        <IterationSample />
+        <br />
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/EventPractice2.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const EventPractice2 = () => {
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+
+  const { username, message } = form;
+
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+
+  const onClick = () => {
+    alert(username + ' : ' + message);
+    setForm({
+      username: '',
+      message: '',
+    });
+  };
+  const onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습(함수 컴포넌트)</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="메시지를 작성해주세요"
+        value={message}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+export default EventPractice2;
