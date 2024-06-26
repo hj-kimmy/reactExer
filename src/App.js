db25e488336857ab83330c753eb619fcadd2c129
hj-kimmy <hjsmile0406@gmail.com>
2024-06-26 21:25:27 +0900

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+import Average from "./Average";
+import MyComponent from "./MyComponent";
+import RefSample from "./RefSample";
 
-function App() {
+const App = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <Counter />
+      {visible && <Info />}
+
+      <Average />
+      <MyComponent />
+      <RefSample />
+    </>
   );
-}
+};
 
 export default App;
