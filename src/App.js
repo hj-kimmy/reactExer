ebd998567b0e7399a7618b85f4897dafc521ba00
hj-kimmy <hjsmile0406@gmail.com>
2024-06-27 16:35:31 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
+      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
